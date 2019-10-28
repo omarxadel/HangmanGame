86a13b9341db85ec9b61fec1542ab3899b98a562
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2019-10-29 00:35:01 +0200

    EngineUpdates/WinLogic/WordAppearDeveloped
    You can now win, know your progress in the game, all in the console still, no GUI

--- Hangman.py
+++ Hangman.py
@@ -35,15 +35,60 @@ def letterInWord(letter, word):
             counter += 1
             continue
         else:
-            print(counter)
             return True
         counter = counter + 1
     return False
 
+def wordToLetters(word):
+    counter = 0
+    letters = []
+    while(counter<len(word)):
+        if word[counter] not in letters:
+            letters.append(word[counter])
+        counter += 1
+    return letters
+
+def printWelcome(word):
+    print("Your word is: ", end="")
+    counter = 0
+    while(counter < len(word)-1):
+        print("_ ",end="")
+        counter += 1
+    print("_")
+
+def printGuessed(guessed, word):
+    counter = 0
+    while(counter < len(word)):
+        if(word[counter] in guessed):
+            print(word[counter], end= "")
+        else:
+            print("_ ", end= "")
+        counter += 1
+
 def engine():
+    lives = 6
     gameWord = randomWordGenerator(randomNumberGenerator())
-    userIn = getConsoleInput()
-    print(letterInWord(userIn, gameWord))
+    gameWordLetters = sorted(wordToLetters(gameWord))
+    inLetters = []
+    printWelcome(gameWord)
+    while(lives >= 0):
+        
+        userIn = getConsoleInput()
+        if(letterInWord(userIn, gameWord) and inLetters.count(userIn) == 0):
+            inLetters.append(userIn)
+            print("Good Job!, Guess another one!")
+        elif(inLetters.count(userIn) > 0):
+            print("Already guessed it!")
+        else:
+            lives -= 1
+            print("Unfortunate! You Got " + str(lives) + " Lives Only!")
+        
+        printGuessed(inLetters, gameWord)
+        inLetters = sorted(inLetters)
+        if(inLetters == gameWordLetters):
+            print("YOU WON!")
+            break
+        
 
 engine()
 
